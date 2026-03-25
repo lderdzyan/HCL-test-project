337c7ff8bca02917666a44278f0c0178acf52a7f
lderdzyan <lderdzyan@vtgsoftware.com>
2026-03-25 16:27:27 +0400

    Has syntax errors

--- deployments/backend/locals.tf
+++ deployments/backend/locals.tf
@@ -1,7 +1,7 @@
 locals {
   config = yamldecode(file("${path.module}/config.yaml"))
 
-  environment = local.config["env"]["environment"]
+  environment = local.config["environment"]
 
-  lambda_map = { for l in local.config["lambda-functions"] : l.name => l }
+  lambda_map = { for l in local.config["lambda_functions"] : l.name => l }
 }
