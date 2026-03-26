50021d861b2ed16c7c608fcf8136a2b04f4dd3f2
lderdzyan <lderdzyan@vtgsoftware.com>
2026-03-26 16:10:37 +0400

    Env name change

--- deployments/backend/locals.tf
+++ deployments/backend/locals.tf
@@ -1,7 +1,7 @@
 locals {
   config = yamldecode(file("${path.module}/config.yaml"))
 
-  environment = local.config["environment"]
+  environment = var.environment
 
   lambda_map = { for l in local.config["lambda_functions"] : l.name => l }
 }
